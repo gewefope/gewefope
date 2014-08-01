86588659a950167d385456d3dbb76c6639511d1a
skhokhlov <worldfly@yandex.com>
2014-08-01 17:10:39 +0400

    Fix bug in b-user-profile. Update package.json.

--- dist/js/global.js
+++ dist/js/global.js
@@ -339,7 +339,7 @@ lego.userInfo_init = function () {
 //    }
     $('<div class="b-user__item b-user__avatar b-user__avatar_small"><img class="b-user__avatar__img" src="http://www.gravatar.com/avatar/' + localStorage.getItem('emailHash') + '?s=30&d=mm"/></div>' +
         '<div class="b-user__item b-user__name">' + localStorage.getItem('displayname') + '</div>' +
-        '<div class="b-user__item b-user__profile"><a class="b-link b-user__profile__link" href="/profile">Profile</a></div>' +
+        '<div class="b-user__item b-user__profile b-user__profile_header"><a class="b-link b-user__profile__link" href="/profile">Profile</a></div>' +
         '<div class="b-user__item b-user__logout"><a class="b-link b-user__logout__link" href="/logout">Logout</a></div>').appendTo('.b-user');
 
     $('.b-user__logout__link').on('click', function () {
